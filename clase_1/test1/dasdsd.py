b469394bb0fef4363d52228241e7ac9b4c70107a
KrezQS26 <chrisalek_pj@hotmail.com>
2020-08-07 15:21:49 -0500

    holahola

--- clase_1/test1/dasdsd.py
+++ clase_1/test1/dasdsd.py
@@ -1,3 +1,9 @@
-asdsdas
-asdasdas
-dasdasd
+
+texto = input("Ingrese un texto: ")
+
+def imprimir_mensaje():
+    print("Gracias por ingresar el texto: " + (texto))
+
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje()
